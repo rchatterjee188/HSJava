2dc2c84e9b03397ff9670dad839da25c6934983d
rchatterjee188 <rchatterjee188@gmail.com>
2017-10-26 17:50:27 -0400

    Add files via upload

---
+++ RC_TicketMachineTester.java
@@ -0,0 +1,17 @@
+
+/**
+ * Tests the method added to RC_TicketMachine
+ * 
+ * Ruhika Chatterjee
+ * 2/27/17
+ */
+public class RC_TicketMachineTester
+{
+    public static void main (String args [])
+    {
+        RC_TicketMachine busFare = new RC_TicketMachine (50);
+        busFare.showPrice();
+    }
+}
+
+// The price of a ticket is 50 cents.
